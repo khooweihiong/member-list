d78ffc39ea22860ee4b1130914127e2ce300e205
Khoo Wei Hiong, GfK <WeiHiong.Khoo@gfk.com>
2015-09-01 14:36:19 +0800

    Add in submit and remove functions

--- src/script.js
+++ src/script.js
@@ -1,1 +1,21 @@
-angular.module('app.memberList', []);
+var app = angular.module('app.memberList', []);
+
+app.controller('MemberController', ['$scope', '$filter', function($scope, $filter) {
+  $scope.member = {};
+  $scope.memberlist = [];
+    
+  $scope.submit = function () {
+    $scope.member.id = $scope.memberlist.length+1;
+    $scope.member.date = $filter('date')(new Date(), 'dd/MM/yyyy');
+    $scope.member.time = $filter('date')(new Date(), 'HH:mm:ss');
+    $scope.memberlist.push($scope.member);
+    $scope.member = {};
+  };
+    
+  $scope.remove = function (id) {
+    _.remove($scope.memberlist, function(n) {
+                            return n.id === id;
+                        });     
+  };
+    
+}]);
